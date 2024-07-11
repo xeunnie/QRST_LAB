bd4daab13db06530ecb9a37a033638776f4b11b1
shinebyul <qufdl8382@gmail.com>
2024-07-12 00:09:00 +0900

    [feat] nav avtive inactive 기능 추가가

--- src/app/component/navigation/nav.js
+++ src/app/component/navigation/nav.js
@@ -1,14 +1,43 @@
+"use client"
+
 import Link from 'next/link'
+import { useState } from 'react';
+import { Space_Grotesk } from 'next/font/google'
+
+const space_grotesk600 = Space_Grotesk({ weight: '600', subsets: ['latin'] });
 
 export default function Nav(){
+
+  const [activeButton, setActiveButton] = useState('');
+
+  const clickNavButton=(buttomName)=>{
+    setActiveButton(buttomName);
+  }
+
     return(
         <div>
           <header>
-            <nav>
-              <Link href="/">about</Link>
-              <Link href="/pages/work">work</Link>
-              <Link href="/pages/service">service</Link>
-              <Link href="/pages/contact">contact</Link>
+            <nav className={space_grotesk600.className}>
+              <Link href="/" 
+                className={activeButton==='active' ? 'active':''}
+                onClick={()=>clickNavButton('active')}>
+                  about
+              </Link>
+              <Link href="/pages/work" 
+                className={activeButton==='work' ? 'active':''}
+                onClick={()=>clickNavButton('work')}>
+                  work
+              </Link>
+              <Link href="/pages/service" 
+                className={activeButton==='service' ? 'active':''}
+                onClick={()=>clickNavButton('service')}>
+                  service
+              </Link>
+              <Link href="/pages/contact" 
+                className={activeButton==='contact' ? 'active':''}
+                onClick={()=>clickNavButton('contact')}>
+                  contact
+              </Link>
             </nav>
           </header>
         </div>
