b7e31df2f1a1ffc8e2312483596ecd7f8a68d343
chloe_choi <plumxeun@gmail.com>
2024-11-18 23:04:06 +0900

    deployment check

--- next.config.js
+++ next.config.js
@@ -5,14 +5,14 @@ const nextConfig = {
     reactStrictMode: false,
     images: {
         domains: ['images.ctfassets.net'],
-      },
-      webpack: (config) => {
+    },
+    webpack: (config) => {
         config.module.rules.push({
-          test: /\.svg$/,
-          use: ['@svgr/webpack'],
-        })
-        return config
-      },
-}
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
